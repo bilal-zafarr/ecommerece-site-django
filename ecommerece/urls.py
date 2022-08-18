3ab2fde3858021bd413a8ad428e08187e571daf0
Bilal Zafar <bilalzaafar@gmail.com>
2022-08-18 20:25:05 +0500

    jazzmin and custom admin site implemented

--- ecommerece/urls.py
+++ ecommerece/urls.py
@@ -17,10 +17,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from store.admin import store_admin_site
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("store.urls")),
+    path("store-admin/", store_admin_site.urls),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/admin.py
+++ store/admin.py
@@ -9,3 +9,19 @@ admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)
+
+
+class StoreAdminSite(admin.AdminSite):
+    site_header = "Store Admin"
+    site_title = "Store Portal"
+    index_title = "Welcome to My Ecommerece"
+
+
+store_admin_site = StoreAdminSite(name="store_admin")
+
+
+store_admin_site.register(Customer)
+store_admin_site.register(Product)
+store_admin_site.register(Order)
+store_admin_site.register(OrderItem)
+store_admin_site.register(ShippingAddress)
