f436208d83d5294a6ec4e4f1748e327455cc89a8
Bilal Zafar <bilalzaafar@gmail.com>
2022-08-19 20:34:44 +0500

    minor improvements

--- store/views.py
+++ store/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import JsonResponse
 from django.shortcuts import redirect, render
 
 from .models import *
